519a55304cfd7e4d3b54c1af37cfe8459cad6f2e
Peter T <pthompso@unca.edu>
2020-02-17 18:59:10 -0500

    user-login.py is the action for the user login html form

---
+++ user-login.py
@@ -0,0 +1,8 @@
+'''
+this file holds the logic to enable users to log in, including
+ - getting user input from html form
+ - querying the database
+ - hashing user passwords
+ - comparing the hash digests
+ - ...
+'''
