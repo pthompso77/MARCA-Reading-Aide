6c8c47621fde5252533ef503523183fb51b06c8b
pthompso77 <pthompso@unca.edu>
2020-02-18 06:53:38 -0500

    connecting to DB with procedure call

--- user-login.py
+++ user-login.py
@@ -11,6 +11,7 @@ this file holds the logic to enable users to log in, including
 
 import cgi, cgitb; cgitb.enable()
 import os, traceback, sys
+import pthompsoDB as db
 
 
 # references HTML field storage element (placeholder) for later retrieval
@@ -23,8 +24,17 @@ nameOfThisFile = "user-login.py"
 username_in = html_form.getvalue('username_in')
 password_in = html_form.getvalue('password_in')
 
+messageOut = 'got username = {} and password = {}'.format(username_in, password_in)
 
-messageOut = 'got username = {} and password {}'.format(username_in, password_in)
+
+""" connecting to DB """
+dbConnect = db.getDBConnection()
+cursor = dbConnect.cursor()
+procName = 'getPassword_andSalt_ByUsername'
+procArgs = (username_in)
+
+dbResult = cursor.callproc(procName, procArgs)
+
 
 print('''Content-type: text/html\r\n\r\n
         <!doctype html>
@@ -36,5 +46,6 @@ print('''Content-type: text/html\r\n\r\n
 	<h1>Success??</h1>
 	<p id="success_YN">
     {message}
+    {dbResult}
 	</p>
-</body>'''.format(message=messageOut))
+</body>'''.format(message = messageOut, dbResult = dbResult))
