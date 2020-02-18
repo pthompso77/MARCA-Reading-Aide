b3160a09414c6a8e2d502736611c847a72d4f7b4
Peter T <pthompso@unca.edu>
2020-02-18 05:38:43 -0500

    login.html is now login.py

---
+++ login.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+print('Content-type: text/html\r\n\r\n')
+print('''<!doctype html>
+<head>
+	<title>Login</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1>Let's Log In!</h1>
+	<form id="user-login-form" action="user-login.py">
+	    <label for="username_in">Username</label>
+	    <input type="text" id="uname" name="username_in"><br><br>
+	    
+	    <label for="password_in">Password</label>
+	    <input type="password" id="pwd" name="password_in"><br><br>
+	    
+	    <input type="submit" value="Submit">
+
+	</form>
+</body>''')
