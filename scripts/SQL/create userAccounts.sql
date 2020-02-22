44eb5ee900b1bcc651f4b4335ba4228d6a77b60c
pthompso77 <pthompso@unca.edu>
2020-02-22 14:46:58 -0500

    dropped 'username' column and commented out old script

--- scripts/SQL/create userAccounts.sql
+++ scripts/SQL/create userAccounts.sql
@@ -1,4 +1,4 @@
--- DROP TABLE `userAccounts;
+/* -- DROP TABLE `userAccounts;
 CREATE TABLE `userAccounts` (
   `email` varchar(2000) NOT NULL,
   `username` varchar(2000) NOT NULL,
@@ -7,16 +7,21 @@ CREATE TABLE `userAccounts` (
   `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+*/
 
 
 -- 02-22
 -- DROP TABLE `userAccounts`;
 CREATE TABLE `userAccounts` (
   `email` varchar(2000) NOT NULL,
-  `username` varchar(2000) NOT NULL,
+  -- DROPPED 02-22 `username` varchar(2000) NOT NULL,
   `password` varchar(2000) NOT NULL,
   `NaCl` varchar(2000) NOT NULL,
   `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `sessionID` varchar(2000) DEFAULT NULL,
   PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- DON'T NEED USERNAME
+ALTER TABLE `pthompsoDB`.`userAccounts` 
+DROP COLUMN `username`;
