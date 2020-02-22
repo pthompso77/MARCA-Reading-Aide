b8b8bacd66af88f80a4ea60c501999cdb4508ec4
pthompso77 <pthompso@unca.edu>
2020-02-22 17:43:01 -0500

    hash and verify hashed passwords with salt

--- crypt.py
+++ crypt.py
@@ -1,62 +1,62 @@
-
 import hashlib, os, binascii
+from hmac import compare_digest
 
 
-def hash_password(password):
-    """Hash a password for storing."""
-    salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
-    pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), 
-                                salt, 100000)
-    pwdhash = binascii.hexlify(pwdhash)
-    return (salt + pwdhash).decode('ascii')
-
-def verify_password(stored_password, provided_password):
-    """Verify a stored password against one provided by user"""
-    salt = stored_password[:64]
-    stored_password = stored_password[64:]
-    pwdhash = hashlib.pbkdf2_hmac('sha512', 
-                                  provided_password.encode('utf-8'), 
-                                  salt.encode('ascii'), 
-                                  100000)
-    pwdhash = binascii.hexlify(pwdhash).decode('ascii')
-    return pwdhash == stored_password
-
-
-
-def hashPass(password):
-    ''' from https://www.vitoshacademy.com/hashing-passwords-in-python/ '''
-    password = bytes(password, 'ascii')
-    rand = os.urandom(60)
-    salty = hashlib.sha256(rand).hexdigest().encode('ascii')
-    print('type 1:', type(password), type(salty))
-    pwdHash = hashlib.pbkdf2_hmac('sha512', password, salty, 1000)
-    pwdHash = binascii.hexlify(pwdHash)
-    pwdHash = (pwdHash + salty).decode('ascii')
-    salty = salty.decode('ascii')
-    return (pwdHash, salty)
+def hash_password(password, optionalSalt_str = -1):
+    """Hash a password for storing in database.
+    Uses hashlib.pbkdf2_hmac sha256 with blocksize=32, parallelization=1
     
+    Parameters:
+        password (str): plaintext password
+        optionalSalt_str: optionally provide the salt to use when hashing [default = -1]
+        (optionalSalt_str will be generated if not provided)
+        
+    Returns:
+        tuple: 
+            [0] hash digest of password (str),
+            [1] salt used to hash the password (str)
+    
+    """
+    block_size = 32
+    parallelization_factor = 1
+    saltIsProvided = not (optionalSalt_str == -1)
+    if saltIsProvided:
+        salt_bytes = bytes(optionalSalt_str, 'utf-8')
+    else: #TODO need to check if this is the right encoding?
+        salt_bytes = hashlib.sha256(os.urandom(60)).hexdigest().encode('utf-8')        
+    salt_str = salt_bytes.decode('utf-8')
+    password_bytes = bytes(password, 'utf-8')
+    pwdhash = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt_bytes, 100000)
+    pwdhash2 = hashlib.scrypt(password=password_bytes, salt=salt_bytes, n=16, r=block_size,
+                             p=parallelization_factor)
+    pwdhash_str = pwdhash.hex()
+    return pwdhash_str, salt_str
 
-def verifyHashPass(passwordFromDB, passwordToVerify):
-    """Verify a stored password against one provided by user"""
-    #salt = passwordFromDB[:64]
-    salt = passwordFromDB[1]
-    passwordFromDB = passwordFromDB[0]
-    passwordToVerify = passwordToVerify.encode('utf-8')
-    print('type 2:',type(passwordToVerify), type(salt))
-    pwdHash = hashlib.pbkdf2_hmac('sha512', passwordToVerify, salt.encode('ascii'), 100000)
-    pwdHash2 = binascii.hexlify(pwdHash).decode('ascii')
-    return pwdHash2 == passwordFromDB    
+
+def verify_password(stored_password_hash, stored_salt_str, provided_password):
+    """Verifies a plaintext password matches the hash digest of a stored password
+    
+    Parameters:
+        stored_password_hash (str): hash digest of stored password
+        stored_salt_str (str): the salt parameter that was used to create the stored_password_hash
+        provided_password (str): plaintext password to verify
+    
+    Returns:
+        True if the passwords match, False if not
+
+    """
+    isMatch = False
+    stored_pw_hash_Bytes = bytes(stored_password_hash, 'utf-8')
+    provided_pwHash = hash_password(provided_password, optionalSalt_str=stored_salt_str)[0]
+    isMatch = compare_digest(stored_password_hash, provided_pwHash)
+    return isMatch
+
+
+def doTesting():
+    testpass = 'Hello!77459*Bye'
+    hashed = hash_password(testpass)
 
 
 if __name__ == "__main__":
-    testpass = 'Hello!77459*Bye'
-    hashed = hashPass(testpass)
-    hashed2 = hash_password(testpass)
-    
-    
-    print('hashed password: ',hashed)
-    print('hashed password2: ',hashed2)
-    
-    print(verifyHashPass(hashed, testpass))
-    print(verify_password(hashed2, testpass))
+    doTesting()
     
