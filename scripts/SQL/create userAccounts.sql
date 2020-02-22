fbc1f23d46b8b2f596bbd1c96b9d8f623efe452b
pthompso77 <pthompso@unca.edu>
2020-02-22 14:24:39 -0500

    DB changes - VARCHARS to VARCHAR(2000)

--- scripts/SQL/create userAccounts.sql
+++ scripts/SQL/create userAccounts.sql
@@ -1,14 +1,22 @@
+-- DROP TABLE `userAccounts;
 CREATE TABLE `userAccounts` (
-  `email` varchar(255) NOT NULL,
-  `username` varchar(16) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  `NaCl` varchar(32) NOT NULL,
+  `email` varchar(2000) NOT NULL,
+  `username` varchar(2000) NOT NULL,
+  `password` varchar(2000) NOT NULL,
+  `NaCl` varchar(2000) NOT NULL,
   `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
--- then...
-
-ALTER TABLE `pthompsoDB`.`userAccounts` 
-ADD COLUMN `sessionID` VARCHAR(2000) NULL AFTER `create_time`;
+-- 02-22
+-- DROP TABLE `userAccounts`;
+CREATE TABLE `userAccounts` (
+  `email` varchar(2000) NOT NULL,
+  `username` varchar(2000) NOT NULL,
+  `password` varchar(2000) NOT NULL,
+  `NaCl` varchar(2000) NOT NULL,
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `sessionID` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
