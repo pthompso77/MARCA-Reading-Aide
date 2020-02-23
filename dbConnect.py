01103d2101fe56b95dc2acc9cd95fe522471cbcf
pthompso77 <pthompso@unca.edu>
2020-02-22 19:53:38 -0500

    fixed some typos

--- dbConnect.py
+++ dbConnect.py
@@ -16,17 +16,17 @@ tokenDocs_pickle_COLUMN = 'TokenizedDocs_Pickledcol'
 
 def getDBConnection(printing = False):
     try:
+        print(host, user, pw, schema)
         dbConnect = MySQLdb.connect(host, user, pw, schema)
         if printing:
-            rint('success to {} on AVL'.format(schema))
+            print('success to {} on AVL'.format(schema))
     except Exception as e:
         if printing:
             print('\nerror in connecting to {}:{}'.format(schema,e))
         errMessage = '[error getting keyWordsPhrases]\n' + str(e)    
-    
+        dbConnect = None
     return dbConnect
 
-connection = getDBConnection()
 
 def runGetQuery(connection, getQuery, printing=False):
     cursor = connection.cursor()
@@ -41,10 +41,15 @@ def runGetQuery(connection, getQuery, printing=False):
     
 def runSetQuery(connection, setQuery):
     cursor = connection.cursor()
-    cursor.execute(getQuery)
+    cursor.execute(setQuery)
     connection.commit()
     connection.close()
     
 if (__name__ == '__main__'):
-    runGetQuery(connection,"SELECT * FROM userAccounts")
-    
+    connection = getDBConnection()    
+    snoutput = runGetQuery(connection,"SELECT * FROM userAccounts")
+    print('result type:',type(snoutput))
+    print('result type[0]:',type(snoutput[0]))
+    for each in snoutput:
+        print(each)
+    pause = True
