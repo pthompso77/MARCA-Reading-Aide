456166560f5df8552d05f1f362c4127c6261c22c
pthompso77 <pthompso@unca.edu>
2020-02-23 07:21:46 -0500

    trying some XHR

--- register.py
+++ register.py
@@ -10,6 +10,24 @@ print('''
 <head>
 	<title>Register</title>
 	<meta charset="utf-8">
+    	<script>
+            function getTasks() {
+                //put more processing in the function as needed
+                var xmlhttp;
+                var parameters = "This must be a string which will be the parameters you will receive in your python script";
+                var scriptName = "text.py";
+                //may be .cgi as well depending on how you are using it
+                xmlhttp = new XMLHttpRequest();
+                xmlhttp.open("POST", scriptName, true);
+                xmlhttp.onreadystatechange = function () {
+                    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                        //retrieve a json response and parse it into userTasks
+                        usersTasks = JSON.parse(xmlhttp.responseText);
+                    }
+                }
+                xmlhttp.send(parameters);
+            }
+	</script>
 </head>
 ''')
 
