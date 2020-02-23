1cbee5fabbcbeba0fb8a3bb1888e761661372f79
pthompso77 <pthompso@unca.edu>
2020-02-23 10:48:34 -0500

    clean up ignored files

--- myDBConnect.py
+++
@@ -1,9 +0,0 @@
-"""
-moving DB connection strings to separate file 
-for security, do not upload files with actual values! 
-(ﾉﾟ0ﾟ)ﾉ~
-"""
-host = 'host.name.here'
-user ='usernameHere'
-pw = 'woops'
-schema = 'schemaName'
