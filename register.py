59fce8e4b13f5c0d6b03ba1fb514012af726ff93
pthompso77 <pthompso@unca.edu>
2020-02-23 07:06:37 -0500

    new registration page with form

---
+++ register.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+print('Content-type: text/html\r\n\r\n')
+
+
+
+print('''<!doctype html>
+<head>
+	<title>Register</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1>Register</h1>
+        <form id="user-register-form" action="" method="post">
+	    <label for="username_in">Username</label>
+	    <input type="text" id="uname" name="username_in"><br><br>
+
+	    <label for="password_in">Password</label>
+	    <input type="password" id="pwd" name="password_in"><br><br>
+
+	    <input type="submit" value="Submit">
+
+	</form>
+</body>''')
