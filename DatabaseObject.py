778d5ffba11f4b65a2652000703a637672729201
pthompso77 <pthompso@unca.edu>
2020-02-23 17:54:03 -0500

    finished runSetQuery change

--- DatabaseObject.py
+++ DatabaseObject.py
@@ -35,7 +35,7 @@ class userAccount(DatabaseObject):
         self.__putInDB()
     def __putInDB(self):
         query = "INSERT INTO {tablename} ('email','password','NaCl','sessionID') VALUES ('{email}','{password}','{NaCl}','{sessionID}')".format(tablename=self._tableName,**self._rowVals)
-        dbConnect.runSetQuery(self._myDBconnection,
+        dbConnect.runSetQuery(query)
     def updateDB(self, newValues_dict):
         #TODO make sure newValues has only one value? maybe...
         items = list(newValues_dict.items())
