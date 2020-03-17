f715419f65a4ac159bab9f0666d13985e6fb1d69
pthompso77 <pthompso@unca.edu>
2020-03-17 12:41:10 -0400

    removing DB connect...again...

--- myDBConnect.py
+++ myDBConnect.py
@@ -3,8 +3,8 @@ moving DB connection strings to separate file
 for security, do not upload files with actual values!
 (ﾉﾟ0ﾟ)ﾉ~
 """
-host = 'avl.cs.unca.edu'
-user ='pthompso'
-pw = 'NEWpassword4me'
-schema = 'pthompsoDB'
+host = 'hostname'
+user ='username'
+pw = 'password'
+schema = 'schemaname'
 
