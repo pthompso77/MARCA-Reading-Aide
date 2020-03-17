5df8bcb1331292704f8e2f20b6f8bfcebc40cf29
pthompso77 <pthompso@unca.edu>
2020-03-17 12:55:00 -0400

    removing DB connect...again...

--- myDBConnect.py
+++ myDBConnect.py
@@ -3,8 +3,8 @@ moving DB connection strings to separate file
 for security, do not upload files with actual values!
 (ﾉﾟ0ﾟ)ﾉ~
 """
-host = 'hostname'
-user ='username'
-pw = 'password'
-schema = 'schemaname'
-
+from realDBConnect import myCreds
+host = myCreds['host']
+user = myCreds['user']
+pw = myCreds['pw']
+schema = myCreds['schema']
