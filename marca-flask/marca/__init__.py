1d763562f0baa166197f096945ac040820c588fe
pthompso77 <pthompso@unca.edu>
2020-03-19 05:37:45 -0400

    initial hello world commit

--- marca-flask/marca/__init__.py
+++ marca-flask/marca/__init__.py
@@ -6,15 +6,16 @@ from flask import Flask
 def create_app(test_config=None):
     #: create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    #: set some default configuration that the app will use
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY='dev', #TODO set to a random value when deploying
         DATABASE = os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
-    if test_config is None:
+    if test_config is None: # overrides the default configuration
         #: load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
-    else:
+    else: #tests can be configured independently of any development values
         #: load the test config if supplied above
         app.config.from_mapping(test_config)
 
@@ -27,7 +28,8 @@ def create_app(test_config=None):
     # a simple page to say Helloooo!
     @app.route('/hello')
     def hello():
-        return 'Helloooo Woooorld!'
+        whom = 'Wooorld'
+        return f'Helloooo {whom}!'
 
     return app
 
