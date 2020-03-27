0f990620cd3a6aedba7ea72ad4e3db3239972e57
pthompso77 <pthompso@unca.edu>
2020-03-27 07:49:21 -0400

    getting back up to speed

---
+++ marca-flask/marca/myDBConnect.py
@@ -0,0 +1,22 @@
+"""
+moving DB connection strings to separate file
+for security, do not upload files with actual values!
+(ﾉﾟ0ﾟ)ﾉ~
+"""
+try:
+    print('trying: `from marca.realDBConnect import myCreds`')
+    from marca.realDBConnect import myCreds
+    print('success')
+except Exception as e:
+    print('failed')
+    print('trying: `from realDBConnect import myCreds`')
+    try:
+        from realDBConnect import myCreds
+        print('success')
+    except Exception as e:
+        print('still no go, E: ',e)
+
+host = myCreds['host']
+user = myCreds['user']
+pw = myCreds['pw']
+schema = myCreds['schema']
