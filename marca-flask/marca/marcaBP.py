0bb7361eb55ca001a68f1bdc4ec63384ebb5c09e
pthompso77 <pthompso@unca.edu>
2020-04-05 13:20:31 -0400

    adding logic to  view to submit FullText and UserID to DB (and association table)

--- marca-flask/marca/marcaBP.py
+++ marca-flask/marca/marcaBP.py
@@ -50,6 +50,25 @@ http_notFound = 404
 http_forbidden = 403
 
 
+@bp.route('/dev/db', methods=(GET, POST))
+def devDB():
+    log.info('Starting def devDB()')
+    db = get_db().connect()
+    cur = db.cursor()
+
+    if request.method==POST:
+        query = request.form['query']
+        DBexec = cur.execute(query)
+        db.commit()
+        DBresult = cur.fetchall()
+
+        return render_template('dev/db.html',query=query, DBexec=DBexec, DBresult=DBresult)
+
+    log.info('Finished def devDB()')
+    #return redirect(url_for('index'))
+    return render_template('dev/db.html')
+
+
 '''=============================INDEX VIEW============================='''
 
 # Database table names
@@ -78,16 +97,81 @@ def dashboard():
     # TODO
     #: get user id
     userID = g.user['userID']
+    error = None
 
+
+    ''' (as a logged-in user)
+        Submit a new text document that will be uploaded to DB and available
+        to review and interact with.
+
+    request form name = newFullText
+    '''
+    #TODO
     #if method is POST
-        #: get the text input
-        #: process the new text (tokenize and summarize)
-        #: send text and userID to DB (make sure they are associated)
+    if request.method == POST:
+        # Get the text form submitted
+        fullText = request.form['newFullText']
+
+        #: tokenize the text
+        from marca.text_tools.my_tokenize import word_tokenize
+        #tokenizedText = str(word_tokenize(fullText))
+        tokenizedText = word_tokenize(fullText)
+
+        # query to call the MySQL proc call
+        # like: CALL `pthompsoDB`.`insert_FullText`(<{IN tokenizedText TEXT}>, <{OUT output INT}>);
+        #insertTextQuery = f'''CALL `pthompsoDB`.`insert_FullText` ("{tokenizedText}", );'''
+
+        db = get_db().connect()
+        cur = db.cursor()
+        result = ""
+
+        tokenizedTexta = "this is a test"
+        #insertTextQuery = f'''CALL `pthompsoDB`.`insert_FullText` ("{tokenizedText}", );'''
+        #>>> args = (5, 6, 0) # 0 is to hold value of the OUT parameter pProd
+        #args = (tokenizedTexta, 0)
+        #>>> cursor.callproc('multiply', args)
+        #result, tokenizedTextID = cur.callproc('insert_FullText', args)
+
+
+        #: submit to DB, get return value (tokenizedTextID)
+        query = f'''INSERT INTO `FullText` (full_text) VALUES ("{tokenizedText}");'''
+        result = cur.execute(query)
+        db.commit()
+        q1 = '''SELECT FullText_ID from pthompsoDB.FullText ORDER BY FullText_ID DESC LIMIT 1;'''
+        cur.execute(q1)
+        tokenizedTextRow =  cur.fetchone()
+        tokenizedTextID = tokenizedTextRow['FullText_ID']
+
+        #: get userAccountsID from g.user
+        userAccountsID = userID
+
+        # insert statement for text/user association
+        fullText_UserAccount_assoc_Query = f'''INSERT INTO `pthompsoDB`.`user_FullText_assoc`
+                (`userID`,
+                `FullTextID`)
+            VALUES
+                ({userAccountsID},
+                {tokenizedTextID});
+            '''
+        cur.execute(fullText_UserAccount_assoc_Query)
+        db.commit()
+        log.info('Finished submitText() in marcaBP.py')
+
+
+# Submit text and userID to DB (make sure they are associated)
+
+        flash('Document Uploaded Successfully')
+        if userID == 29: # (then it's me testing)
+            flash(f'got (first 20): {fullText[:20]}')
+            flash(f'''and tokenized (first 1)= {tokenizedText[:1]}''')
+            flash(f'''and userAccountsID= {userAccountsID}''')
+            flash(f'''and result= {result}''')
+            flash(f'''and tokenizedTextID= {tokenizedTextID}''')
 
 
     # continues if POST, jumps here if GET
     #: select texts from DB that match user
-    userTexts = ['get','fromDB']
+    userTexts = ['TODO', 'get','fromDB']
 
 
     #: render the dashboard template, send the user Texts also
@@ -111,6 +195,7 @@ def review():
 
 
 '''=======================Actions======================='''
+
 
 # TODO
 @bp.route('/submitText', methods=(POST,))
